0fd30c6a95455e377b5d455994808e6dd4b6fcd6
Amine KAROUI <aminkarouidev@gmail.com>
2023-10-04 00:35:01 +0200

    Testing web component

---
+++ public/vocables/js/sourceSelector.js
@@ -0,0 +1,22 @@
+// html and component are the basic required imports
+import { html } from "https://cdn.jsdelivr.net/npm/lit-html@2.8.0/lit-html.min.js";
+import { component, useState } from 'https://cdn.skypack.dev/pin/haunted@v5.0.0-FvCc6Fq9BO6lNOEjq7Jg/mode=imports/optimized/haunted.js';
+import "./button.js" ;
+import "./input.js" ;
+// import from "../css/main.css";
+// Create your functional component, and return a lit-html TemplateResult
+function sourceSelector() {
+  // create the DOM elements
+
+  
+  return html`
+  <link rel="stylesheet" href="http://localhost:3010/vocables/css/main.css">
+  <slsv-button>Lineage</slsv-button>
+  <slsv-input></slsv-input>
+  `;
+}
+
+// Register your element to custom elements registry, pass it a tag name and your class definition
+// Note how we're wrapping our function in a `component()` function
+// The element name must always contain at least one dash
+customElements.define("slsv-sourceSelector", component(sourceSelector));
---
+++ public/vocables/js/tool.js
@@ -0,0 +1,20 @@
+// html and component are the basic required imports
+import { html } from "https://cdn.jsdelivr.net/npm/lit-html@2.8.0/lit-html.min.js";
+import { component, useState } from 'https://cdn.skypack.dev/pin/haunted@v5.0.0-FvCc6Fq9BO6lNOEjq7Jg/mode=imports/optimized/haunted.js';
+// import from "../css/main.css";
+// Create your functional component, and return a lit-html TemplateResult
+function BasicSetup() {
+  // create the DOM elements
+
+  
+  return html`
+  <link rel="stylesheet" href="http://localhost:3010/vocables/css/main.css">
+  <div>
+  <h1 class="abcde" style="color:red">regergregre</h1>
+  </div>`;
+}
+
+// Register your element to custom elements registry, pass it a tag name and your class definition
+// Note how we're wrapping our function in a `component()` function
+// The element name must always contain at least one dash
+customElements.define("basic-setup", component(BasicSetup));
