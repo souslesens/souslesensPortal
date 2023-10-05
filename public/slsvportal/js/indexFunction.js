88fe1964d10aaf3ce76ebba8be816b3c756753f5
Amine KAROUI <aminkarouidev@gmail.com>
2023-10-05 16:59:04 +0200

    Haunted Web Component / Lit-Html Web component seperate

--- public/slsvportal/js/indexFunction.js
+++ public/slsvportal/js/indexFunction.js
@@ -1,10 +1,10 @@
-import {LitElement, html,css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
-import { component } from 'https://cdn.skypack.dev/pin/haunted@v5.0.0-FvCc6Fq9BO6lNOEjq7Jg/mode=imports/optimized/haunted.js';
+// import {LitElement, html,css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+import { component, html } from 'https://cdn.skypack.dev/pin/haunted@v5.0.0-FvCc6Fq9BO6lNOEjq7Jg/mode=imports/optimized/haunted.js';
 // A powerful feature of lit-html is that templates are just variables.
 // You can create them anywhere, and you can define generic templates
 // to be used by multiple components.
 
-
+// HAUNTED WEB COMPONENT
 
 function SourceSelector  (){
 
