be264daafd1e7d325047c1c2c165effc7ffa6b1f
Amine KAROUI <aminkarouidev@gmail.com>
2023-10-13 15:48:13 +0200

    update

--- public/vocables/modules/uiWidgets/sourceSelectorWidget copy.js
+++ public/vocables/modules/uiWidgets/sourceSelectorWidget copy.js
@@ -1,7 +1,14 @@
 import MainController from "../shared/mainController.js";
+import SearchUtil from "../search/searchUtil.js";
 import common from "../shared/common.js";
 import {LitElement, html,css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
 import JstreeWidget from "./jstreeWidget.js";
+
+//import { component, html, useEffect } from "https://cdn.skypack.dev/pin/haunted@v5.0.0-FvCc6Fq9BO6lNOEjq7Jg/mode=imports/optimized/haunted.js";
+//import  "./indexSeperatedFiles/slsvbutton.js";
+//import  "./indexSeperatedFiles/slsvinput.js";
+
+
 /**
  * ***********************************************************************
  *  Widget >Description
@@ -59,13 +66,60 @@ return html`
 firstUpdated() {
   super.firstUpdated();
   var shadowDom=this.renderRoot.querySelector('#sourceSelector');
-  $("#mainDialogDiv").html(shadowDom);
-
-  SourceSelectorWidget.initJsTree();
-
+  $("#graphDiv").html(shadowDom);
 }
 }
   customElements.define("slsv-source-selector", sourceSelector);
+// function sourceSelector({ abc, cancel }) {
+//     useEffect(() => {
+//         test();
+//       }, []);
+    
+//       const test = () => {
+//         console.log("Testing function");
+//         return html`<div id="sourceSelector" style="margin-bottom: 10px; width: auto; min-height: 0px; max-height: none; height: 680px">
+//         <div class="sourceSelector_buttons">
+//             <button class="btn btn-sm my-1 py-0 btn-outline-primary" onclick="$('#sourceSelector').dialog('close')">Cancel</button>
+//             <button class="btn btn-sm my-1 py-0 btn-outline-primary" id="sourceSelector_validateButton">OK</button>
+//         </div>
+//         <div>Search : <input id="sourceSelector_searchInput" value="" autocomplete="off" style="width: 200px; font-size: 12px; margin: 3px; padding: 3px" /></div>
+    
+//         <div class="jstreeContainer" style="width: 360px; height: 600px; overflow: auto; margin-top: 5px">
+//             <div id="sourceSelector_jstreeDiv"></div>
+//         </div>
+//     </div>
+//     `;
+//       };
+//   const handleClick = () => {
+//     alert("../../vocables/snippets/sourceSelector.html");
+
+//   };
+
+//   return html`
+//    <!-- <link rel="stylesheet" href="http://localhost:3010/slsvportal/assets/css/main.css">-->
+//     <div id="sourceSelector" style="margin-bottom: 10px; width: auto; min-height: 0px; max-height: none; height: 680px">
+//       <div class="sourceSelector_buttons">
+//         <button class="btn btn-sm my-1 py-0 btn-outline-primary" onclick="$('#sourceSelector').dialog('close')">Cancel</button>
+//         <button class="btn btn-sm my-1 py-0 btn-outline-primary" id="sourceSelector_validateButton">OK</button>
+//       </div>
+//       <div>Search : <input id="sourceSelector_searchInput" value="" autocomplete="off" style="width: 200px; font-size: 12px; margin: 3px; padding: 3px" /></div>
+
+//       <div class="jstreeContainer" style="width: 360px; height: 600px; overflow: auto; margin-top: 5px">
+//         <div id="sourceSelector_jstreeDiv"></div>
+//       </div>
+//     </div>
+
+//   `;
+
+// }
+
+// sourceSelector.observedAttributes = ["abc", "cancel"];
+// // Register your element to custom elements registry, pass it a tag name and your class definition
+// // Note how we're wrapping our function in a `component()` function
+// // The element name must always contain at least one dash
+// customElements.define("slsv-source-selector", component(sourceSelector));
+
+
 /**
  * **********************************************************************************************
  * Business logic
@@ -78,55 +132,7 @@ firstUpdated() {
 var SourceSelectorWidget = (function() {
   var self = {};
   self.currentTreeDiv = null;
-  self.initJsTree = function(){
-    var options = {
-      includeSourcesWithoutSearchIndex: true,
-      withCheckboxes: true,
-  };
-  var selectTreeNodeFn = function () {
-      $("#mainDialogDiv").dialog("close");
-      var checkedSource = SourceSelectorWidget.getCheckedSources();
-  
-      if (checkedSource.length > 0) {
-          return;
-      }
-      var source = SourceSelectorWidget.getSelectedSource()[0];
-      self.mainSource = source;
-      self.setCurrentSource(source);
-      $("#sourcesSelectionDialogdiv").dialog("close");
-      $("#lineage_allActions").css("visibility", "visible");
-      MainController.UI.showHideRightPanel("show");
-  };
-  
-  var validateButtonFn = function () {
-      var sources = SourceSelectorWidget.getCheckedSources();
-      self.loadSources(sources);
-  };
-   SourceSelectorWidget.loadSourcesTreeDiv("sourceSelector_jstreeDiv", options);
-    $("#sourceSelector_searchInput").focus();
-    //  $("#sourceSelector_SearchSourceInput");
-    var isDialog = true;
-    $("#sourceSelector_validateButton").bind("click", function() {
-      okButtonValidateFn();
-      
-      if (isDialog) {
-        $("#" + targetDivId).dialog("close");
-      }
-    });
-  
-    if (options.withCheckboxes) {
-      $(".sourceSelector_buttons").css("display", "block");
-    }
-    else {
-      $(".sourceSelector_buttons").css("display", "none");
-    }
-    
-    if (false) {
-      $("#" + targetDivId).dialog("open");
-    }
-    else {
-    }
-  }
+
   self.initWidget = function(types, targetDivId, isDialog, selectTreeNodeFn, okButtonValidateFn, options) {
     if (self.currentTreeDiv != null) {
       if ($("#" + self.currentTreeDiv).jstree() != undefined) {
@@ -143,8 +149,37 @@ var SourceSelectorWidget = (function() {
     var jsTreeOptions = options;
     jsTreeOptions.selectTreeNodeFn = selectTreeNodeFn;
     MainController.UI.showHideRightPanel("hide");
-  $("#mainDialogDiv").dialog("open");
-  $("#mainDialogDiv").html("<slsv-source-selector></slsv-source-selector>");
+
+  $("#graphDiv").html("<slsv-source-selector></slsv-source-selector>");
+
+
+
+//  $("#graphDiv").load("<slsv-source-selector></slsv-source-selector>", function(err) {
+
+
+//       self.loadSourcesTreeDiv("sourceSelector_jstreeDiv", jsTreeOptions);
+//       $("#sourceSelector_searchInput").focus();
+//       //  $("#sourceSelector_SearchSourceInput");
+//       $("#sourceSelector_validateButton").bind("click", function() {
+//         okButtonValidateFn();
+//         if (isDialog) {
+//           $("#" + targetDivId).dialog("close");
+//         }
+//       });
+
+//       if (options.withCheckboxes) {
+//         $(".sourceSelector_buttons").css("display", "block");
+//       }
+//       else {
+//         $(".sourceSelector_buttons").css("display", "none");
+//       }
+
+//       if (isDialog) {
+//         $("#" + targetDivId).dialog("open");
+//       }
+//       else {
+//       }
+//   });
 
 };
 
