067f42467178a90986fcfb9a32b2c95760c5472c
Amine KAROUI <aminkarouidev@gmail.com>
2023-10-21 01:59:18 +0200

    Widget Functions

--- public/sense/components/homeWidget.js
+++ public/sense/components/homeWidget.js
@@ -1,5 +1,5 @@
 import {LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
-
+import  '../../vocables/modules/uiWidgets/js/basicComponents.js';
 
 
 /**
@@ -31,11 +31,14 @@ import {LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit
 class Widget extends LitElement {
     static properties = {
         title: { type: String, reflect: true },
+        classWidget: { type: String, reflect: true },
+        widgetId: { type: String, reflect: true },
       };
       constructor() {
         super();
         this.title = "";
-
+        this.classWidget = "";
+        this.widgetId = "";
         this.attachShadow({ mode: 'open' });
     }
   
@@ -43,7 +46,7 @@ class Widget extends LitElement {
       return html`
         <div class="widget">
           <p>${this.title}</p>
-          <button @click="${this.navigateTo}">Go to ${this.title}</button>
+          <slsv-button btnId="${this.widgetId}" btnClass="${this.classWidget}" value="${this.title}" @click="${this.navigateTo}"></slsv-button>
         </div>
       `;
     }
